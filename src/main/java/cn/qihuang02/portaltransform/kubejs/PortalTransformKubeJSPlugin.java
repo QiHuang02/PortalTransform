e5e4b9ab2c460628d40d85b417674a066f99a88a
QiHuang02 <128583144+QiHuang02@users.noreply.github.com>
2025-09-11 00:19:47 +0800

    Use KubeJS build 16 and update plugin API

--- src/main/java/cn/qihuang02/portaltransform/kubejs/PortalTransformKubeJSPlugin.java
+++ src/main/java/cn/qihuang02/portaltransform/kubejs/PortalTransformKubeJSPlugin.java
@@ -1,14 +1,14 @@
 package cn.qihuang02.portaltransform.kubejs;
 
-import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
-import dev.latvian.mods.kubejs.recipe.schema.RecipeSchemaRegistry;
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
 
 /**
  * KubeJS integration plugin for PortalTransform.
  */
-public class PortalTransformKubeJSPlugin implements KubeJSPlugin {
+public class PortalTransformKubeJSPlugin extends KubeJSPlugin {
     @Override
-    public void registerRecipeSchemas(RecipeSchemaRegistry registry) {
-        registry.namespace("portaltransform").register("item_transform", ItemTransformRecipeJS.SCHEMA);
+    public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
+        event.namespace("portaltransform").register("item_transform", ItemTransformRecipeJS.SCHEMA);
     }
 }
